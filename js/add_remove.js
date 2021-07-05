31a227fb14fb9c2317082e4e5e2eca7d37fb9222
ahzia <ah.ziayosfi@gmail.com>
2021-07-05 17:03:42 +0430

    configure local storage to store books

--- js/add_remove.js
+++ js/add_remove.js
@@ -1,6 +1,6 @@
 /**
- * Create a collection that keeps a list of books (hint: you can use an array of objects for that).
-Create a function to add a new book to the collection, with title and author.
+ *  Create a collection that keeps a list of books (hint: you can use an array of objects for that). - done
+Create a function to add a new book to the collection, with title and author. -done
 Create a function to remove a book from the collection (hint: you can use
 the array filter() method).
 Display all books saved in the collection in the top part of the page.
@@ -10,21 +10,21 @@ The new book is displayed in the page.
 Make sure that when a user clicks the "Remove" button:
 The correct book is removed from the collection.
 The correct book dissapears from the page.
-Make sure that data is preserved in the browser's memory by using localStorage.
- */
-
+Make sure that data is preserved in the browser's memory by using localStorage.*/
 /*   Create a collection that keeps a list of books
-    (hint: you can use an array of objects for that).    */
-const books = [
-  { title: "Lorem Ipsun", author: "Testeroo Testyy" },
-  { title: "Second book", author: "Testeroo Testyy" },
-];
-
+    (hint: you can use an array of objects for that).*/
+    var books = JSON.parse(window.localStorage.getItem('books'));
+    if (books === null) {
+      //initial values
+      books= [{ title: "Lorem Ipsun", author: "Testeroo Testyy" },
+         { title: "Second book", author: "Testeroo Testyy" },
+     ];
+     window.localStorage.setItem('books',JSON.stringify(books));
+    }
 function displayBooks() {
   const section = document.getElementById("collection");
   const list = document.createElement("ul");
   list.id = "list";
-
   books.forEach((b) => {
     let { title } = b;
     title = title.replace(/\s/g, "");
@@ -39,6 +39,10 @@ function displayBooks() {
 
   section.innerHTML = "";
   section.appendChild(list);
+}
+
+function searchForBook(title){
+  
 }
 
 function addBook() {
@@ -65,19 +69,19 @@ function addBook() {
       }
       return 0;
     });
+    window.localStorage.setItem('books',JSON.stringify(books));
     displayBooks();
   }
 }
 
 function removebook(data) {
+  console.log(data.id)
   const idBtn = data.id;
+
   const liId = `li${idBtn}`;
   const li = document.getElementById(liId);
   const ul = document.getElementsByTagName("ul");
   ul[0].removeChild(li);
-
-  // const index = books.findIndex((b) => b.title === title);
-  // if (index !== -1) books.splice(index, 1);
 }
 
 // This first function ensures that the document has being already created
