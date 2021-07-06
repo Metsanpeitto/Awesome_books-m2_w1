a3f334f810c2e5a2ea0f1f24b230e417423cb8d1
Metsanpeitto <abraham@waldenberginc.com>
2021-07-06 10:28:16 +0200

    Fix README file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Awesome books: plain JavaScript with objects
+# Awesome books: refactor to use JavaScript classes
 
 ![Alt text](images/screenshot.png?raw=true 'Optional Title')
 
@@ -10,16 +10,14 @@
 ### Instructions
 
 - Deploy your website using GitHub Pages.
-- Check the online version of your portfolio and make sure that the page works properly.
 - Update the README of your repository to include a link to the online version.
 
 =======
 
 ## Built With
 
-- HTML/SCSS
+- HTML/CSS
 - Node.JS
-- Figma
 
 =======
 
@@ -31,30 +29,17 @@ If you want to visit the deployed app just visit the following link:
 
 If you want a copy of this file go to the github repository and download it
 
-- `git clone https://github.com/Metsanpeitto/Awesome_books-m2_w1`
+- `git clone https://github.com/Metsanpeitto/Awesome_books-m2_w1`(https://github.com/Metsanpeitto/Awesome_books-m2_w1)
 
 After cloning it you can see the project opening the index.html file with your favorite browser ( Firefox for example )
 
 
 ### Project requirements
 
-- Implement only a basic UI with plain HTML:
-<p align="center">
-  <img src="./images/awesome_books_basic_ui.png" alt="Basic UI"  width="300px"/>
-</p>
-
-- Create a collection that keeps a list of books (hint: you can use an array of objects for that).
-- Create a function to add a new book to the collection, with title and author.
-- Create a function to remove a book from the collection (hint: you can use the [array filter()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) method).
-- Display all books saved in the collection in the top part of the page.
-- Make sure that when a user clicks the "Add" button:
-  - A new book is added to the collection.
-  - The new book is displayed in the page.
-- Make sure that when a user clicks the "Remove" button:
-  - The correct book is removed from the collection.
-  - The correct book dissapears from the page.
-- Make sure that data is preserved in the browser's memory by using [localStorage](https://github.com/microverseinc/curriculum-html-css/blob/main/local_storage.md).
-- To debug your application, you can use [console.log()](https://developer.mozilla.org/en-US/docs/Web/API/Console/log). **Remember to remove debugging code before pushing your code to the remote repository.**
+- Use JavaScript `classes` instead of `objects` and `arrays`.
+  - You can use objects and arrays, but do it *inside* classes.
+- Create class `methods` to add and remove books.
+- Maintain all of the original functionality of the application.
 - Don't use any library or framework, use only plain JavaScript.
 -  **You need to use pair-programming for this project. We will check to see if both partners submitted code by looking at the commit history for each project. Therefore you need to use only one repository for your pair-programming group.**
 
--- js/class_add_remove.js
+++ js/class_add_remove.js
@@ -1,43 +1,47 @@
+/**       Bookcollection is the main class in this application       */
 class BookCollection {
   constructor() {
     this.books = null;
   }
 
+  /**       Display books is used to show the book collection      */
   displayBooks() {
-    const section = document.getElementById("collection");
-    const list = document.createElement("ul");
-    list.id = "list";
+    const section = document.getElementById('collection');
+    const list = document.createElement('ul');
+    list.id = 'list';
     if (this.books) {
       this.books.forEach((book) => {
         let { title } = book;
-        title = title.replace(/\s/g, "_");
+        title = title.replace(/\s/g, '_');
         const liId = `li${title}`;
         const bookCard = `<li id=${liId}>
             <h5>${book.title}</h5>
             <h6>${book.author}</h6>
             <button id=${title} onclick="bookCollection.removebook(${title})" class="remove">Remove</button>
           </li>`;
-        list.insertAdjacentHTML("beforeend", bookCard);
+        list.insertAdjacentHTML('beforeend', bookCard);
       });
-      section.innerHTML = "";
+      section.innerHTML = '';
       section.appendChild(list);
     }
   }
 
+  /**       UpdateLocalStorage saves and retrieves from local storage       */
   updateLocalStorage(remove) {
     if (remove !== true) {
       if (this.books === null) {
-        this.books = JSON.parse(window.localStorage.getItem("books"));
+        this.books = JSON.parse(window.localStorage.getItem('books'));
       }
     }
 
-    window.localStorage.setItem("books", JSON.stringify(this.books));
+    window.localStorage.setItem('books', JSON.stringify(this.books));
     this.displayBooks();
   }
 
+  /**       Addbook adds books to the book collection       */
   addBook() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
     let found = null;
     if (!this.books) {
       this.books = [];
@@ -67,8 +71,9 @@ class BookCollection {
     }
   }
 
+  /**       Removebook removes the selected book from the collection       */
   removebook(data) {
-    const title = data.id.replace(/_/g, " ");
+    const title = data.id.replace(/_/g, ' ');
     const temp = [];
     this.books.forEach((book) => {
       if (book.title !== title) {
@@ -79,24 +84,25 @@ class BookCollection {
     this.updateLocalStorage(true);
   }
 }
-
+/**       Initialize the class before calling its methods       */
 const bookCollection = new BookCollection();
 
 // This first function ensures that the document has being already created
-document.addEventListener("DOMContentLoaded", () => {
-  const btnAdd = document.getElementById("add-btn");
+document.addEventListener('DOMContentLoaded', () => {
+  const btnAdd = document.getElementById('add-btn');
   btnAdd.addEventListener(
-    "click",
-    function () {
+    'click',
+    () => {
       bookCollection.addBook();
     },
-    false
+    false,
   );
-  const removeBtn = document.getElementsByClassName("remove-btn");
+  const removeBtn = document.getElementsByClassName('remove-btn');
   if (removeBtn.length !== 0) {
     removeBtn.forEach((button) => {
-      button.addEventListener("click", bookCollection.removebook, false);
+      button.addEventListener('click', bookCollection.removebook, false);
     });
   }
 });
+/**       This method is called when the application runs for first time       */
 bookCollection.updateLocalStorage(false);
